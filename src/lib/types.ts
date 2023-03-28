3d8da7ba9fc46b3941bced001d1c00fc71a6fb6b
Bartek Telec <bartektelec@gmail.com>
2023-03-28 13:40:58 +0200

    fix: :wheelchair: improve a11y, use CSS prefixed classes, add event types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,20 @@ export interface Column<T = unknown> {
 	sort?: (a: T, b: T) => number;
 	sortable?: boolean;
 }
+
+export type ColumnClickEvent<T> = CustomEvent<{
+	event: Event;
+	column: Column<T>;
+	columnIndex: number;
+}>;
+
+export type RowClickEvent<T> = CustomEvent<{ event?: Event; row: T; rowIndex: number }>;
+
+export type CellClickEvent<T> = CustomEvent<{
+	event: Event;
+	row: T;
+	rowIndex: number;
+	column: Column<T>;
+	columnIndex: number;
+	cell: string | number | boolean;
+}>;
